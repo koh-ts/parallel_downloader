41a0473386ac1d204307e36dbd3ba53217256f01
koh-ts <k0702611@gmail.com>
2020-07-09 22:48:20 +0900

    added thread function

--- thread_parallel_downloader.c
+++ thread_parallel_downloader.c
@@ -8,23 +8,49 @@
 #include <signal.h>
 #include <sys/wait.h>
 #include <ncurses.h>
+#include <pthread.h>
 
 #define BUF_LEN 1024
 #define URL_LEN 256
+#define MAX_FILES 10
 
-void sigchld_handler(int x){
-    int chld;
-    chld = waitpid(-1, NULL, WNOHANG);
-    if (chld == -1)
-		perror("wait error");
-		exit(EXIT_FAILURE);
-    signal(SIGCHLD, sigchld_handler);
+struct arg {
+	char* ch;
+	int idx;
+};
+
+int f[MAX_FILES];
+char c[MAX_FILES][BUF_LEN];
+int cnt;
+
+void progress(){ //display current progress
+	static int pos = 0;
+	static char mark[] = {'-','\\','|','/'};
+    int k = 0;
+	for(int i = 0; i < 1000000; ++i) {
+        k = 0;
+        clear();
+        for(int j = 0; j < cnt; j++){
+            mvprintw(3 * j, 0, "\r%s", c[j]);
+            if(f[j] == 1) mvprintw(3 * j + 1, 0, "\r%c", mark[pos]);
+            else{
+                mvprintw(3 * j + 1, 0, "\r%s", "finishedddd!");
+                k++;
+            }
+        }
+		refresh();
+		pos = (pos + 1) % sizeof(mark);
+		usleep(100000);
+        if(k == cnt) pthread_exit((void *) 0);
+	}
 }
 
 int http_spliturl(char* url, char* host, char* path, char* filename, unsigned short* port){
     char *p, host_path[URL_LEN];
     if ( strlen(url) > URL_LEN-1 ) {
-        fprintf(stderr,"URL is too long\n"); return 1;
+		endwin();
+        perror("URL is too long\n");
+		exit(EXIT_FAILURE);
     }
     if ( strstr(url, "http://") && sscanf(url, "http://%s", host_path) ) {
         if ( (p = strchr(host_path,'/')) != NULL ) {
@@ -46,13 +72,20 @@ int http_spliturl(char* url, char* host, char* path, char* filename, unsigned sh
         }
         if ( *port <= 0 ) *port = 80;
     } else {
-        fprintf(stderr,"URL has to be begun by http://\n");
-        return 1;
+		endwin();
+        perror("URL has to be begun with http://\n");
+        exit(EXIT_FAILURE);
     }
-    return 0;
+	return 0;
 }
 
-void child_task(char* url, int cnt){
+void child_task(void *tmp){
+    pthread_detach(pthread_self());
+	struct arg *info;
+	info = tmp;
+	char *url = info->ch;
+	int id = info->idx;
+	f[id] = 1;
 	int fd, s, i = 0;
 	long int diff = 0;
 	char *p;
@@ -60,36 +93,35 @@ void child_task(char* url, int cnt){
 	struct hostent *servhost;
 	struct sockaddr_in server;
 	unsigned short port = 80;
-
-	if(http_spliturl(url, host, path, filename, &port)){
+	int fuck;
+	if(fuck = http_spliturl(url, host, path, filename, &port)){
+		endwin();
+		printf("fuck is %d\n", fuck);
 		perror("failed to parse URL");
 		exit(EXIT_FAILURE);
 	}
-
-	// printf("\nhost = %s\n", host);
-	// printf("path = %s\n", path);
-	// printf("filename = %s\n", filename);
-
 	servhost = gethostbyname(host);
 	if(servhost == NULL) {
+		endwin();
 		perror("failed to conversion to IP address.");
         exit(EXIT_FAILURE);
 	}
-	// printf("IP Address %d.%d.%d.%d\n", (u_char)servhost->h_addr[0], (u_char)servhost->h_addr[1], (u_char)servhost->h_addr[2], (u_char)servhost->h_addr[3]);
-	bzero(&server, sizeof(server));
+	memset(&server, 0, sizeof(server));
 	server.sin_family = AF_INET;
 	bcopy(servhost->h_addr, &server.sin_addr, servhost->h_length);
 	server.sin_port = htons(port);
  
 	if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		endwin();
 		perror("failed to create a socket."); //check if socket is created
         exit(EXIT_FAILURE);
 	}
 
 	if ( connect(s, (struct sockaddr *)&server, sizeof(server)) == -1) {
+		endwin();
 		perror("failed to connect."); //check connection
         exit(EXIT_FAILURE);
-	}
+	}	
 
 	sprintf(send_buf, "GET %s HTTP/1.0\r\n", path);
 	write(s, send_buf, strlen(send_buf));
@@ -102,6 +134,7 @@ void child_task(char* url, int cnt){
 
 	fd = open(filename, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU|S_IRGRP|S_IXGRP|S_IROTH|S_IXOTH);
 	if (fd < 0) {
+		endwin();
 		perror("open error\n");
 		exit(EXIT_FAILURE);
 	}
@@ -122,57 +155,64 @@ void child_task(char* url, int cnt){
 					}
 				} else {
 					break;
-					printf("http error\n");
 				}
 			} else write(fd, buf, read_size);
 		} else break;
 		i++;
 	}
-	
 	close(fd);
 	close(s);
+    f[id] = 0;
+	pthread_exit((void *) 0);
 }
 
+
 int main(int argc, char *argv[]){
-	initscr();
-	if (argc != 2) {
+    initscr();
+    clear();
+    if (argc != 2) {
+		endwin();
 		perror("Input error.\n");
         exit(EXIT_FAILURE);
 	}
 
-	FILE *fp;
-	int cnt = 0, pid;
+    FILE *fp;
 	char buf[BUF_LEN];
  
 	fp = fopen(argv[1], "r");
 	if(fp == NULL) {
-		printf("%s file not open!\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
+	cnt = 0;
 	while (fgets(buf, BUF_LEN, fp) != NULL) {
     	cnt++;
   	}
-	char c[cnt][BUF_LEN];
-	fseek(fp, 0L, SEEK_SET);
-	for(int i = 0; i < cnt && fgets(c[i], sizeof(c[i]), fp) != NULL;i++){
-		mvprintw(i, 0, "list[%d]=%s", i, c[i]);
-		sleep(1);
-		refresh();
+	// cnt--;
+	mvprintw(10, 0, "%d is cnt\n", cnt);
+	refresh();
+    for(int i = 0; i < cnt; i++){
+        memset(&c[i], 0, sizeof(c[i]));
 	}
+    fseek(fp, 0L, SEEK_SET);
+	for(int i = 0; i < cnt && fgets(c[i], sizeof(c[i]), fp) != NULL;i++){
+        f[i] = 1;
+    }
 	fclose(fp);
-	sleep(3);
-	endwin();
-
-	signal(SIGCHLD, sigchld_handler);
-	for (int j = 0; j < cnt; ++j) {
-		pid = fork();
-		if (pid == 0) {
-			child_task(c[j], cnt);
-			_exit(0);
+    
+    pthread_t thread[cnt], pro;
+	struct arg tmp[cnt];
+    pthread_create(&pro, NULL, (void *)progress, NULL);
+	for(int i = 0; i < cnt; i++){
+        tmp[i].ch = c[i];
+		tmp[i].idx = i;
+        sleep(1);
+        if((pthread_create(&thread[i], NULL, (void *)child_task, (void *) &tmp[i]) != 0)){
+			endwin();
+			perror("pthread_create error");
+			exit(EXIT_FAILURE);
 		}
-  	}
-	wait(NULL);
-	exit(0);
-	// endwin();
+	}
+	sleep(3);
+	pthread_join(pro, NULL);
+    endwin();
 }
